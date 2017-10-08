6848dcf37e3a2e36db08a99e6e82056c3263dbb2
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-10-08 22:52:19 +0600

    Added a lot of coding channels.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-	var sources = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "noopkat", "dreamleague", "throwdowntv", "Omgitsfirefoxx", "kindafunnygames", "manvsgame", "trihex", "nightblue3", "kittyplays"];
+	var sources = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "noopkat", "dreamleague", "throwdowntv", "Omgitsfirefoxx", "kindafunnygames", "manvsgame", "trihex", "nightblue3", "kittyplays", "open_mailbox", "danrovito", "handmade_hero", "tsoding", "rw_grim", "daroou2", "nmarulo", "mikeconley_dot_ca", "vinlam", "gaprogman", "codedependant", "blocksandgold", "syntag", "devwars", "krzjn", "drathy", "pajlada", "abnercoimbre", "automateallthethings", "loonygeekfun"];
 	var endPoint = "https://wind-bow.glitch.me/twitch-api/";
 	var channelDataAPICall = endPoint+"channels/";
 	var channelStreamAPICall = endPoint+"streams/";
