4dc4e6dbea6a78cc7043c483bd77ac31d2eeec42
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-10-08 15:39:27 +0600

    Now fetching both, channel data and stream data.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,76 @@
 $(document).ready( function() {
-	var sources = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "noopkat"];
+	var sources = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "noopkat", "dreamleague", "throwdowntv", "Omgitsfirefoxx", "kindafunnygames", "manvsgame", "trihex", "nightblue3", "kittyplays"];
 	var endPoint = "https://wind-bow.glitch.me/twitch-api/";
-	var htmlCard = ""
-								+"Channel Name"
-								+"";
+	var channelDataAPICall = endPoint+"channels/";
+	var channelStreamAPICall = endPoint+"streams/";
+	var currentChannel;
+	var isLive;
 
 	function magic() {
 		for (i=0; i < sources.length; i++) {
-			var channelDataAPICall = endPoint+"channels/"+sources[i];
-			console.log(channelDataAPICall);
+			channelDataAPICall = endPoint+"channels/"+sources[i];
 
 			$.ajax({
 				url: channelDataAPICall,
 				type: "GET",
 				dataType: "jsonp",
 				success: function(channelData) {
+					currentChannel = channelData.name;
 					console.log(channelData);
-					$(".channels").append(
-						"<div class='card-container'><div class='card'><div class='channel-logo'><img src='"
-						+channelData.logo+"'></div><div class='channel-info'><a href='"+channelData.url+
-						"' class='channel-name' target='_blank'>"+channelData.display_name+"</a><p class='stream-status'>"+
-						"Offline</p><p class='channel-status'>"+channelData.status+"</p></div></div></div>"
-						);
+					$.ajax({
+						url: endPoint+"streams/"+currentChannel,
+						type: "GET",
+						dataType: "jsonp",
+						success: function(streamData) {
+							console.log(streamData);
+							if (streamData.stream == null) {
+								isLive = false;
+							} else {
+								isLive = true;
+							}
+							console.log(isLive);
+
+							if (isLive) {
+								$(".channels").append(
+								"<div class='card-container'><div class='card-online'><div class='channel-logo'><img src='"
+								+channelData.logo+"'></div><div class='channel-info'><a href='"+channelData.url+
+								"' class='channel-name' target='_blank'>"+channelData.display_name+"</a><p class='stream-active'>"+
+								"Online</p><p class='channel-status'><span>Now Playing: </span>"+streamData.stream.channel.status+"</p></div></div></div>"
+								);
+							} else {
+								$(".channels").append(
+								"<div class='card-container'><div class='card-offline'><div class='channel-logo'><img src='"
+								+channelData.logo+"'></div><div class='channel-info'><a href='"+channelData.url+
+								"' class='channel-name' target='_blank'>"+channelData.display_name+"</a><p class='stream-inactive'>"+
+								"Offline</p><p class='channel-status'>Last Status: "+channelData.status+"</p></div></div></div>"
+								);
+							}
+						}
+					})
 				}
 			})
 		}
 	}
+
+		$(".all").click( function(){
+			$(".switches").find("p").removeClass("active");
+			$(".all").addClass("active");
+			$(".card-container").fadeOut().delay(500).fadeIn();
+		});
+
+		$(".online").click( function(){
+			$(".switches").find("p").removeClass("active");
+			$(".online").addClass("active");
+			$(".card-container").fadeOut().delay(500);
+			$(".card-online").parent().fadeIn();
+		});
+
+		$(".offline").click( function(){
+			$(".switches").find("p").removeClass("active");
+			$(".offline").addClass("active");
+			$(".card-container").fadeOut().delay(500);
+			$(".card-offline").parent().fadeIn();
+		});
 
 	magic();
 });
